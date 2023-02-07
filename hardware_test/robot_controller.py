47ad7fe332f643856ce708aea4ed6eedbb527377
Lina Ismail <ilina19@hotmail.com>
2023-02-07 02:20:11 -0500

    basic gui

---
+++ hardware_test/robot_controller.py
@@ -0,0 +1,363 @@
+import RPi.GPIO as GPIO
+from adafruit_servokit import ServoKit
+import time
+import tkinter
+
+# ===================== hardware =====================
+
+kit = ServoKit(channels=16)
+
+SERV_1 = 0
+SERV_2 = 1
+SERV_3 = 2
+
+BUT_1 = 'UART2_CTS'     # 36
+BUT_2 = 'DAP4_DIN'      # 38
+BUT_3 = 'DAP4_DOUT'     # 40
+
+LED_1 = 'SPI2_MISO'     # 22
+LED_2 = 'SPI1_CS0'      # 24
+LED_3 = 'SPI1_CS1'      # 26
+
+MOT_1 = 'GPIO_PE6'      # 33
+
+IN_A = 'DAP4_SCLK'      # 12
+IN_B = 'GPIO_PZ0'       # 31
+
+MOT_2 = 'LCD_BL_PW'     # 32
+
+IN_C = 'SPI1_MOSI'      # 19
+IN_D = 'CAM_AF_EN'      # 29
+
+
+# ===================== methods =====================
+
+def turn_on_led(compartment):
+    if compartment == '1':
+        print("turning on LED 1")
+        GPIO.output(LED_1, GPIO.HIGH)
+        # GPIO.output(LED_2, GPIO.LOW)
+        # GPIO.output(LED_3, GPIO.LOW)
+
+    elif compartment == '2':
+        print("turning on LED 2")
+        # GPIO.output(LED_1, GPIO.LOW)
+        GPIO.output(LED_2, GPIO.HIGH)
+        # GPIO.output(LED_3, GPIO.LOW)
+
+    elif compartment == '3':
+        print("turning on LED 3")
+        # GPIO.output(LED_1, GPIO.LOW)
+        # GPIO.output(LED_2, GPIO.LOW)
+        GPIO.output(LED_3, GPIO.HIGH)
+
+    else:
+        print("turning off all LEDs")
+        killAllLED()
+
+
+def testLocking():
+    while 1:
+        compartment = input("test Locking (1, 2 or 3)\nu - unlock all\nq - quit\n")
+
+        if compartment == '1':
+            print("locking compartment 1")
+
+            kit.servo[SERV_1].angle = 0
+
+            # time.sleep(2)
+
+            # print("unlocking compartment 1")
+
+            # kit.servo[SERV_1].angle = 180
+                
+        elif compartment == '2':
+            print("locking compartment 2")
+
+            kit.servo[SERV_2].angle = 0
+
+            # time.sleep(2)
+
+            # print("unlocking compartment 1")
+
+            # kit.servo[SERV_2].angle = 180
+            
+        elif compartment == '3':
+            print("locking compartment 3")
+
+            kit.servo[SERV_3].angle = 0
+
+            # time.sleep(2)
+
+            # print("unlocking compartment 1")
+
+            # kit.servo[SERV_3].angle = 180
+
+        elif compartment == 'u':
+            print("unlocking all")
+            killAllServo()
+
+        elif compartment == 'q':
+            break
+
+        else:
+            print("invalid input")
+
+def testPWM():
+    dc=0                               # set dc variable to 0 for 0%
+    
+    pwm1.start(dc)                      # Start PWM with 0% duty cycle
+    pwm2.start(dc)                      # Start PWM with 0% duty cycle
+
+    while 1:
+        # set direction for each motor
+        direction = input("select direction:\n1 - forward\n2 - backward\n3 - turn right\n4 - turn left\nk - kill pwm\nq - quit\n")
+
+        if direction == '1':
+            GPIO.output(IN_A, GPIO.LOW)
+            GPIO.output(IN_B, GPIO.HIGH)
+            GPIO.output(IN_C, GPIO.HIGH)
+            GPIO.output(IN_D, GPIO.LOW)
+
+            speed = input("forward - select speed:\n1 - slow\n2 - cruise\n3 - max\nq - quit\n")
+            if speed == ' 1':
+                dc = 20
+            elif speed == '2':
+                dc = 50
+            elif speed == '3':
+                dc = 80
+            elif speed == 'q':
+                dc = 0            
+
+            input("start test?")
+
+            print(f'sending {dc} to motors 1 and 2')
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+            
+            input("press enter to stop")
+
+            print("stopping motors")
+            dc = 0
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+
+        elif direction == '2':
+            GPIO.output(IN_A, GPIO.HIGH)
+            GPIO.output(IN_B, GPIO.LOW)
+            GPIO.output(IN_C, GPIO.LOW)
+            GPIO.output(IN_D, GPIO.HIGH)
+
+            speed = input("backward - select speed:\n1 - slow\n2 - cruise\n3 - max\nq - quit\n")
+            if speed == ' 1':
+                dc = 20
+            elif speed == '2':
+                dc = 50
+            elif speed == '3':
+                dc = 80
+            elif speed == 'q':
+                dc = 0            
+
+            input("start test?")
+
+            print(f'sending {dc} to motors 1 and 2')
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+            
+            input("press enter to stop")
+
+            print("stopping motors")
+            dc = 0
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+
+        elif direction == '3':
+            GPIO.output(IN_A, GPIO.LOW)
+            GPIO.output(IN_B, GPIO.HIGH)
+            GPIO.output(IN_C, GPIO.HIGH)
+            GPIO.output(IN_D, GPIO.LOW)
+
+            speed = input("turn right - select speed:\n1 - slow\n2 - cruise\n3 - max\nq - quit\n")
+            if speed == ' 1':
+                dc = 20
+            elif speed == '2':
+                dc = 50
+            elif speed == '3':
+                dc = 80
+            elif speed == 'q':
+                dc = 0            
+
+            input("start test?")
+
+            print(f'sending {dc} to motors 1 and 2')
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+            
+            input("press enter to stop")
+
+            print("stopping motors")
+            dc = 0
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+
+        elif direction == '4':
+            GPIO.output(IN_A, GPIO.LOW)
+            GPIO.output(IN_B, GPIO.HIGH)
+            GPIO.output(IN_C, GPIO.HIGH)
+            GPIO.output(IN_D, GPIO.LOW)
+
+            speed = input("turn left - select speed:\n1 - slow\n2 - cruise\n3 - max\nq - quit\n")
+            if speed == ' 1':
+                dc = 20
+            elif speed == '2':
+                dc = 50
+            elif speed == '3':
+                dc = 80
+            elif speed == 'q':
+                dc = 0            
+
+            input("start test?")
+
+            print(f'sending {dc} to motors 1 and 2')
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+            
+            input("press enter to stop")
+
+            print("stopping motors")
+            dc = 0
+            pwm1.ChangeDutyCycle(dc)
+            pwm2.ChangeDutyCycle(dc)
+
+        elif direction == 'k':
+            print("killing pwm")
+            pwm1.ChangeDutyCycle(0)
+            pwm2.ChangeDutyCycle(0)
+
+        elif direction == 'q':
+            pwm1.ChangeDutyCycle(0)
+            pwm2.ChangeDutyCycle(0)
+
+            pwm1.stop()
+            pwm2.stop()
+            break
+
+        else:
+            print("invalid input")
+
+def button1(channel):
+    print("button 1 pressed")
+    kit.servo[SERV_1].angle = 0
+    
+    # GPIO.output(LED_1, GPIO.LOW)
+
+    # time.sleep(1)
+
+    # print("unlocking")
+
+    # kit.servo[SERV_1].angle = 0
+
+def button2(channel):
+    print("button 2 pressed")
+    kit.servo[SERV_2].angle = 0
+    # print("compartment 2 is closed")
+    # print("locking")
+
+    # kit.servo[SERV_2].angle = 180
+    
+    # GPIO.output(LED_2, GPIO.LOW)
+
+    # time.sleep(1)
+
+    # print("unlocking")
+    
+    # kit.servo[SERV_2].angle = 0
+    
+def button3(channel):
+    print("button 3 pressed")
+    kit.servo[SERV_3].angle = 0
+    # print("compartment 3 is closed")
+    # print("locking")
+
+    # kit.servo[SERV_3].angle = 180
+    
+    # GPIO.output(LED_3, GPIO.LOW)
+
+    # time.sleep(1)
+
+    # print("unlocking")
+    
+    # kit.servo[SERV_3].angle = 0
+
+def killAllLED():
+    GPIO.output(LED_1, GPIO.LOW)
+    GPIO.output(LED_2, GPIO.LOW)
+    GPIO.output(LED_3, GPIO.LOW)
+
+def killAllServo():
+    kit.servo[SERV_1].angle = 180
+    kit.servo[SERV_2].angle = 180
+    kit.servo[SERV_3].angle = 180
+
+
+# ===================== setup =====================
+
+GPIO.setmode(GPIO.TEGRA_SOC)
+
+GPIO.setup(BUT_1, GPIO.IN)
+GPIO.setup(BUT_2, GPIO.IN)
+GPIO.setup(BUT_3, GPIO.IN)
+
+GPIO.setup(LED_1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED_3, GPIO.OUT, initial=GPIO.LOW)
+
+
+# MOTORS
+
+GPIO.setup(MOT_1, GPIO.OUT)
+GPIO.setup(MOT_2, GPIO.OUT)
+
+GPIO.setup(IN_A, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN_B, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(IN_C, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(IN_D, GPIO.OUT, initial=GPIO.LOW)
+
+
+pwm1 = GPIO.PWM(MOT_1, 50)   # Initialize PWM on pwmPin 50Hz frequency
+pwm2 = GPIO.PWM(MOT_2, 50)   # Initialize PWM on pwmPin 50Hz frequency
+
+
+GPIO.add_event_detect(BUT_1, GPIO.RISING, callback=button1, bouncetime=1)
+GPIO.add_event_detect(BUT_2, GPIO.RISING, callback=button2, bouncetime=1)
+GPIO.add_event_detect(BUT_3, GPIO.RISING, callback=button3, bouncetime=1)
+
+
+# ===================== main =====================
+
+killAllLED()
+# killAllServo()
+
+while 1:
+    print("which component would you like to test?")
+    choice = input("1: LEDS\n2: Locking\n3: Motors\nq: quit\n")
+
+    if choice == '1':
+        testLED()
+    elif choice == '2':
+        testLocking()
+    elif choice == '3':
+        testPWM()
+    elif choice == 'q':
+        break
+    else:
+        print("incorrect input")
+    
+    
+pwm1.stop()     # stop PWM1
+pwm2.stop()     # stop PWM1
+
+killAllLED()    # turn off LEDs
+
+GPIO.cleanup()
--- hardware_test/test_interface.py
+++ hardware_test/test_interface.py
@@ -1,132 +1,96 @@
-from tkinter import *
+import customtkinter as ctk
+from tkinter import Tk, Frame, Canvas
+from PIL import ImageTk, Image  
+import time
 
-class Page(Frame):
-    def __init__(self, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
-    def show(self):
-        self.lift()
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
-# class nurse_login(Page):
-#    def __init__(self, *args, **kwargs):
-#         Page.__init__(self, *args, **kwargs)
-#         inputtxt = Text(self, height = 5, width = 20)
-#         inputtxt.pack()
+root = ctk.CTk() 
+root.geometry("1024x600")
 
-class compartment_select(Page):
-   def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        text2 = Label(self, text="Select compartment ", bg="#013F78", font=('Verdana, 40'), fg="white")
-        text2.pack(pady=29)
-
-        compartment1_img = PhotoImage(file="Assets/compartment1.png")
-        compartment1_button = Button(image=compartment1_img, bg="#013F78", border=0)
-        compartment1_button.pack()
-
-        compartment2_img = PhotoImage(file="Assets/compartment2.png")
-        compartment2_button = Button(image=compartment2_img, bg="#013F78", border=0)
-        compartment2_button.pack(pady=29)
-
-        compartment3_img = PhotoImage(file="Assets/compartment3.png")
-        compartment3_button = Button(image=compartment3_img, bg="#013F78", border=0)
-        compartment3_button.pack()
-
-class close_compartment(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       text2 = Label(self, text="Please close compartment", bg="#013F78", font=('Verdana, 40'), fg="white")
-       text2.pack(pady=29)
-
-class enjoy_meal(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       text2 = Label(self, text="Enjoy your meal!", bg="#013F78", font=('Verdana, 40'), fg="white")
-       text2.pack(pady=29)
-
-class MainView(Frame):
-    def __init__(self, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
-        # p1 = nurse_login(self)
-        p2 = compartment_select(self)
-        p3 = close_compartment(self)
-        p4 = enjoy_meal(self)
-
-        buttonframe = Frame(self)
-        container = Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
-        container.pack(side="top", fill="both", expand=True)
-
-        # p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p4.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-
-        # b1 = Button(buttonframe, text="Page 1", command=p1.show)
-        b2 = Button(buttonframe, text="Page 2", command=p2.show)
-        b3 = Button(buttonframe, text="Page 3", command=p3.show)
-        b4 = Button(buttonframe, text="Page 4", command=p4.show)
-
-        # b1.pack(side="left")
-        b2.pack(side="left")
-        b3.pack(side="left")
-        b4.pack(side="left")
-
-        p2.show()
-
-if __name__ == "__main__":
-    window = Tk()
-    main = MainView(window)
-    window.title("Mybot Interface")
-    window.configure(background="#013F78")
-    window.minsize(200, 200)
-    window.maxsize(1024, 600)
-    window.geometry("1024x600")
-    window.mainloop()
+def login(frame1, text_box): 
+    input_pass = text_box.get()
+    if input_pass == "0000": 
+        frame1.pack_forget()
+        assign_compartment()
+        print("Test")
+    else:
+        warn = ctk.CTkLabel(master=frame1, text="Wrong password. Try again.", font=("Roboto", 20))
+        warn.pack(pady=10, padx=10)
 
 
+def start_page():
+    nurse_login = ctk.CTkFrame(master=root)
+    nurse_login.pack(pady=20, padx=60, fill="both", expand=True)
 
+    label = ctk.CTkLabel(master=nurse_login, text="Nurse Authentication", font=("Roboto", 40))
+    label.pack(pady=100, padx=10)
 
+    entry1 = ctk.CTkEntry(master=nurse_login, placeholder_text="Password", width=750, height=50, font=("Roboto", 24), show="*")
+    entry1.pack(pady=12, padx=10)
 
-def nurse_input_state():
-    return
+    button = ctk.CTkButton(master=nurse_login, text="Submit", width=200, height=50, font=("Roboto", 20), command=lambda: login(nurse_login, entry1))
+    button.pack(pady=30, padx=10)
 
-def compartment_select_State(): 
-    text2 = Label(window, text="Select compartment ", bg="#013F78", font=('Verdana, 40'), fg="white")
-    text2.pack(pady=29)
-
-    compartment1_img = PhotoImage(file="Assets/compartment1.png")
-    compartment1_button = Button(image=compartment1_img, bg="#013F78", border=0)
-    compartment1_button.pack()
-
-    compartment2_img = PhotoImage(file="Assets/compartment2.png")
-    compartment2_button = Button(image=compartment2_img, bg="#013F78", border=0)
-    compartment2_button.pack(pady=29)
-
-    compartment3_img = PhotoImage(file="Assets/compartment3.png")
-    compartment3_button = Button(image=compartment3_img, bg="#013F78", border=0)
-    compartment3_button.pack()
-
-
-def quitGame(event):
-    window.destroy()
+def compartment_button(frame, button, id): 
+    # turn on LED 
+    # load 
+    # while unlocked: 
+    #     display "Please close compartment" message 
     
+    #unclock compartment 
+    while 1: # while unlocked
+        # open box icon
+        print("button clicked!")
+        button.configure(state="disabled")
+        break
 
-window = Tk()
-window.title("Mybot Interface")
-window.configure(background="#013F78")
-window.minsize(200, 200)
-window.maxsize(1024, 600)
-window.geometry("1024x600")
+        # "Place load in compartment" + cancel 
+        # if locked
+            # closed box icon 
+        # if cancel 
+            # remove open box icon
+            # exit loop
+
+def cancel_load(label, btn): 
+    label.grid_forget() 
+    btn.grid_forget()
 
 
-# #creating background
-# bgImage = ImageTk.PhotoImage(Image.open("Images/background.png")) 
-# bg = canvas.create_image(0, 0, image=bgImage, anchor=tk.NW)
-
-# #creating button which supports png transparency
-# quitImage = ImageTk.PhotoImage(Image.open("Images/quitImage.png"))
-# quitButton = canvas.create_image(50, 50, image=quitImage)
-# canvas.tag_bind(quitButton, "<Button-1>", quitGame)
+def setting_off(frame):
+    frame.pack_forget()
+    set_off()
+    return 
 
 
-# window.attributes("-fullscreen", True)+
-window.mainloop()
+def assign_compartment():
+    compartment_frame = ctk.CTkFrame(master=root)
+    compartment_frame.pack(pady=20, padx=60, fill="both", expand=True)
+    compartment_frame.columnconfigure(index=0, weight=1)
+
+    label = ctk.CTkLabel(master=compartment_frame, text="Load Compartments", font=("Roboto", 40))
+    label.grid(row=0, column=0, pady=30, padx=10)
+
+    comp1_button = ctk.CTkButton(master=compartment_frame, text="1", width=200, height=50, font=("Roboto", 20), command=lambda: compartment_button(compartment_frame, comp1_button, 1))
+    comp1_button.grid(row=1, column=0, pady=30, padx=10)
+
+    comp2_button = ctk.CTkButton(master=compartment_frame, text="2", width=200, height=50, font=("Roboto", 20), command=lambda: compartment_button(compartment_frame, comp2_button, 2))
+    comp2_button.grid(row=2, column=0, pady=30, padx=10)
+
+    comp3_button = ctk.CTkButton(master=compartment_frame, text="3", width=200, height=50, font=("Roboto", 20), command=lambda: compartment_button(compartment_frame, comp3_button, 3))
+    comp3_button.grid(row=3, column=0, pady=30, padx=10)
+
+    submit_button = ctk.CTkButton(master=compartment_frame, text="Submit", width=200, height=50, font=("Roboto", 20), command=lambda: setting_off(compartment_frame))
+    submit_button.grid(row=4, column=0, pady=30, padx=10)
+
+def set_off(): 
+    omw_frame = ctk.CTkFrame(master=root)
+    omw_frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+    label = ctk.CTkLabel(master=omw_frame, text="On my way to patient!", font=("Roboto", 40))
+    label.pack(pady=30, padx=10, fill="x", expand=True)
+
+
+start_page()
+root.mainloop()
